a79aa61af857dc95091c77552765fa8aece2490c
Юрий Андреев <y_andreev@mail.ru>
2022-12-05 21:06:52 +0300

    4-module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,14 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    let tr = table.rows[i];
+    if (tr.cells[3].dataset.available === "true")
+      tr.classList.add("available");
+    else if (tr.cells[3].dataset.available === "false")
+      tr.classList.add("unavailable");
+    else if (tr.cells[3].dataset.available === undefined)
+      tr.setAttribute("hidden", "true");
+    if (tr.cells[2].innerHTML === "m") tr.classList.add("male");
+    else if (tr.cells[2].innerHTML === "f") tr.classList.add("female");
+    if (+tr.cells[1].innerHTML < 18 ) tr.style.cssText = `text-decoration: line-through`
+  }
 }
