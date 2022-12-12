09c9d7e984de8a1906e861f6e0b5bdabfa1a5876
Юрий Андреев <y_andreev@mail.ru>
2022-12-12 18:32:48 +0300

    5 module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,6 @@
 function toggleText() {
-  // ваш код...
+  let elem = document.querySelector('.toggle-text-button');
+  elem.onclick = function(){
+    text.hasAttribute("hidden") ? text.removeAttribute("hidden") : text.setAttribute("hidden", "true");
+  }  
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,24 @@
 function initCarousel() {
-  // ваш код...
+  let arrowRightButton = document.querySelector('.carousel__arrow_right');
+  let arrowLeftButton = document.querySelector('.carousel__arrow_left');
+  let elem = document.querySelector('.carousel__inner');
+  if (!elem.dataset.counter) {
+    elem.setAttribute('data-counter',0);
+    arrowLeftButton.style.display = 'none'
+  }
+
+  arrowRightButton.onclick = function(){
+    let slideWidth = document.querySelector('.carousel__slide').offsetWidth * (Number(elem.dataset.counter) + 1);
+    elem.style.transform = 'translateX(-'+ slideWidth + 'px)';
+    elem.dataset.counter = Number(elem.dataset.counter) + 1;
+    if (Number(elem.dataset.counter) == 3) arrowRightButton.style.display = 'none';
+    if (Number(elem.dataset.counter) > 0 && arrowLeftButton.style.display == 'none' ) arrowLeftButton.style.display = '';
+  }
+  arrowLeftButton.onclick = function(){
+    let slideWidth = document.querySelector('.carousel__slide').offsetWidth * ((1-elem.dataset.counter));
+    elem.style.transform = 'translateX('+ slideWidth + 'px)';
+    elem.dataset.counter = Number(elem.dataset.counter) - 1;
+    if (Number(elem.dataset.counter) == 0) arrowLeftButton.style.display = 'none';
+    if (Number(elem.dataset.counter) < 3 && arrowRightButton.style.display == 'none' ) arrowRightButton.style.display = '';
+  }
 }
