db045152808b8c4df18f72a3b4f2fe673481ea01
Юрий Андреев <y_andreev@mail.ru>
2022-12-14 16:27:49 +0300

    5 module remastered

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,6 +1,6 @@
 function toggleText() {
   let elem = document.querySelector('.toggle-text-button');
   elem.onclick = function(){
-    text.hasAttribute("hidden") ? text.removeAttribute("hidden") : text.setAttribute("hidden", "true");
+    text.hidden = !text.hidden
   }  
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -3,22 +3,24 @@ function initCarousel() {
   let arrowLeftButton = document.querySelector('.carousel__arrow_left');
   let elem = document.querySelector('.carousel__inner');
   if (!elem.dataset.counter) {
-    elem.setAttribute('data-counter',0);
+    elem.setAttribute('data-counter', 0);
     arrowLeftButton.style.display = 'none'
   }
+  function slideOffset(arrowDirection) {
+    let slideWidth = document.querySelector('.carousel__slide').offsetWidth * (arrowDirection * Number(elem.dataset.counter) + 1);
+    elem.style.transform = 'translateX(' + slideWidth * (-arrowDirection) + 'px)';
+    elem.dataset.counter = Number(elem.dataset.counter) + arrowDirection;
 
-  arrowRightButton.onclick = function(){
-    let slideWidth = document.querySelector('.carousel__slide').offsetWidth * (Number(elem.dataset.counter) + 1);
-    elem.style.transform = 'translateX(-'+ slideWidth + 'px)';
-    elem.dataset.counter = Number(elem.dataset.counter) + 1;
     if (Number(elem.dataset.counter) == 3) arrowRightButton.style.display = 'none';
-    if (Number(elem.dataset.counter) > 0 && arrowLeftButton.style.display == 'none' ) arrowLeftButton.style.display = '';
-  }
-  arrowLeftButton.onclick = function(){ 
-    let slideWidth = document.querySelector('.carousel__slide').offsetWidth * ((1-elem.dataset.counter));
-    elem.style.transform = 'translateX('+ slideWidth + 'px)';
-    elem.dataset.counter = Number(elem.dataset.counter) - 1;
+    else arrowRightButton.style.display = '';
+    
     if (Number(elem.dataset.counter) == 0) arrowLeftButton.style.display = 'none';
-    if (Number(elem.dataset.counter) < 3 && arrowRightButton.style.display == 'none' ) arrowRightButton.style.display = '';
+    else arrowLeftButton.style.display = '';
+  }
+  arrowRightButton.onclick = function () {
+    slideOffset(1);
+  }
+  arrowLeftButton.onclick = function () {
+    slideOffset(-1);
   }
 }
